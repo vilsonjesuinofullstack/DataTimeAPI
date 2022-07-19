0d9b0ea89ac5a3fc3681edc32abbb885a30d757f
Vilson Jesuino de Lima Junior <vilsonjesuino@outllook.com.br>
2022-07-19 11:47:43 -0300

    Conhecendo o objeto calendar

--- src/model_date/AppData.java
+++ src/model_date/AppData.java
@@ -2,21 +2,37 @@ package model_date;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class AppData {
 
     public static void main(String[] args) throws ParseException {
 
+        Calendar calendar = Calendar.getInstance();
+
         Date date = new Date();
 
         System.out.println("Data em milisegundos " + date.getTime());
+        System.out.println("Calendar em milisegundos : " + calendar.getTimeInMillis() + "\n");
+
         System.out.println("Dia do mês " + date.getDate());
+        System.out.println("Calendar dia do mês : " + calendar.get(Calendar.DAY_OF_MONTH) + "\n");
+
         System.out.println("Dia da semana " + date.getDay());
+        System.out.println("Calendar dia da semana : " + (calendar.get(Calendar.DAY_OF_WEEK) - 1) + "\n");
+
         System.out.println("Horas do dia " + date.getHours());
+        System.out.println("Calendar horas do dia : " + calendar.get(Calendar.HOUR_OF_DAY) + "\n");
+
         System.out.println("Minutos da hora " + date.getMinutes());
+        System.out.println("Calendar minutos da hora : " + calendar.get(Calendar.MINUTE) + "\n");
+
         System.out.println("Segundos " + date.getSeconds());
+        System.out.println("Calendar segundos : " + calendar.get(Calendar.SECOND) + "\n");
+
         System.out.println("Ano " + (date.getYear() + 1900));
+        System.out.println("Calendar ano : " + calendar.get(Calendar.YEAR) + "\n");
 
         /*-------------------Simples Date Format--------------------*/
 
